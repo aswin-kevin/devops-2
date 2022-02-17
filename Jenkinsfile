6f1e300c2138beca5563722a56b42e48e71b5f43
aswin-kevin <aswinvenkat60@gmail.com>
2022-02-18 02:26:21 +0530

    added all ci/cd scripts

---
+++ Jenkinsfile
@@ -0,0 +1,37 @@
+pipeline {
+    agent { label 'jenkins-slave-1' } 
+    environment {
+		DOCKERHUB_CREDENTIALS=credentials('aswin-dockerhub')
+	}
+    stages {
+        stage('Build output folder') {
+            steps {
+                echo 'Started building new website source code'
+                sh 'sudo npm i && sudo npm run export'
+            }
+        }
+        stage('Build docker image') {
+            steps {
+                echo 'Started building docker image'
+                sh 'sudo docker build -t aswinkevin/kube-server:$BUILD_NUMBER .'
+            }
+        }
+
+        stage('Docker Login') {
+			steps {
+				sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
+			}
+		}
+
+		stage('Docker Push') {
+			steps {
+				sh 'sudo docker push aswinkevin/kube-server:$BUILD_NUMBER'
+			}
+		}
+    }
+    post {
+		always {
+			sh 'docker logout'
+		}
+	}
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -13,12 +13,15 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to{" "}
+          <a target="_blank" href="https://nextjs.org">
+            Kube Server!
+          </a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          This is a kubernetes application{" "}
+          <code className={styles.code}>Version 1.0</code>
         </p>
 
         <div className={styles.grid}>
@@ -53,17 +56,10 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        <div>
+          Powered by <strong> Edureka</strong>
+        </div>
       </footer>
     </div>
-  )
+  );
 }
